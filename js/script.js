b050da3f9a754cf52733c6f5da8be72b1a9c6770
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-16 17:29:49 +0000

    javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Lamees Hemdan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked () {
+  // this program adds sequential numbers up to the user input
+  // input
+  const userNumber = parseInt(document.getElementById("number").value)
+
+  // process
+  let answer = 0
+  for (let Counter = 0; Counter <= userNumber; Counter++) {
+    answer = answer + Counter
+  }
+
+  // output
+  document.getElementById('answer').innerHTML = "The answer is: " + answer
+}
+  
